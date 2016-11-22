1eda4cc1438c5e44e2b9bdc17d695d99739e79c7
Krystyna Malewski <krmalewski@gmail.com>
2016-11-22 12:08:04 -0500

    Get cites rendering on page

--- src/components/SearchList/SearchList.jsx
+++ src/components/SearchList/SearchList.jsx
@@ -5,15 +5,18 @@ import style from './SearchList.css';
 class SearchList extends Component {
 
   renderSearchResults() {
-    // this.props.cityInfo.forEach((city) => {
-    //   console.log(city;
+    console.log('render')
+    if (this.props.cityInfo.length > 0 ) {
+      console.log(this.props.cityInfo[1][0].info.city.name);
+      console.log(this.props.cityInfo[1][0].info.country.name);
+    }
 
-    // return this.props.cityInfo.map((city, i) =>
-    //   <SearchListItem
-    //     key={i}
-    //     city={city.info.city.name}
-    //   />,
-    // );
+    return this.props.cityInfo.map((city, i) =>
+      <SearchListItem
+        key={i}
+        city={city[0].info.city.name}
+      />,
+    );
   }
 
   render() {
