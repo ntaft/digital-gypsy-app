5a97e5ad2469efd661439204de413a4b6f80660a
Krystyna Malewski <krmalewski@gmail.com>
2016-11-22 17:29:49 -0500

    Add city schema

--- db/user_schema.sql
+++ db/user_schema.sql
@@ -1,9 +1,11 @@
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
   username VARCHAR(128) NOT NULL,
   email VARCHAR(128) NOT NULL,
   password VARCHAR(255) NOT NULL,
   createDate TIMESTAMP,
   accessedDate TIMESTAMP
 );
+
