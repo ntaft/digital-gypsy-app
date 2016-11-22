b6a9682cf6a4c1dbf3ebce02dfdd085a76db54dd
Krystyna Malewski <krmalewski@gmail.com>
2016-11-22 12:21:24 -0500

    Render nomad info to page

--- src/components/SearchList/SearchList.jsx
+++ src/components/SearchList/SearchList.jsx
@@ -7,14 +7,23 @@ class SearchList extends Component {
   renderSearchResults() {
     console.log('render')
     if (this.props.cityInfo.length > 0 ) {
-      console.log(this.props.cityInfo[1][0].info.city.name);
-      console.log(this.props.cityInfo[1][0].info.country.name);
+      console.log(this.props.cityInfo[1][0].cost.longTerm.USD);
+      // console.log(this.props.cityInfo[1][0].media.'250');
     }
 
     return this.props.cityInfo.map((city, i) =>
       <SearchListItem
         key={i}
         city={city[0].info.city.name}
+        country={city[0].info.country.name}
+        lat={city[0].info.location.latitude}
+        lng={city[0].info.location.longitude}
+        nomadScore={city[0].scores.nomadScore}
+        wifi={city[0].scores.free_wifi_available}
+        fun={city[0].scores.leisure}
+        safety={city[0].scores.safety}
+        cost={city[0].cost.longTerm.USD}
+
       />,
     );
   }
