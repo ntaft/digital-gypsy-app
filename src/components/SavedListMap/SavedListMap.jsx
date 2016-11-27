02d5d644d938116577cd67f69d4849be5fcededa
Janelle Rosario <janelle.rosario@gmail.com>
2016-11-27 09:06:05 -0500

    design modified and saved list map box added

---
+++ src/components/SavedListMap/SavedListMap.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './SavedListMap.css';
+
+class SavedListMap extends Component {
+
+render() {
+    return (
+      <div><p>map of selected locations</p>
+        <div className="saved-list-map">
+        </div>
+      </div>
+    );
+  }
+}
+
+export default SavedListMap;
